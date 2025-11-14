fca4d107cc7ea50bb4b44ed2d59e19ce4c05e2aa
ncyl3316 <niki2004.chan@gmail.com>
2025-11-15 00:19:59 +0800

    Delete scripts directory

--- scripts/predict.py
+++
@@ -1,4 +0,0 @@
-# Script: predict.py 
-# Author: Your Name 
-# Date: 29-12-2024 
-# Description: A standalone script to generate predictions from trained models. 
--- scripts/train.py
+++
@@ -1,4 +0,0 @@
-# Script: train.py 
-# Author: Your Name 
-# Date: 29-12-2024 
-# Description: A standalone script for training the model with specified parameters. 
