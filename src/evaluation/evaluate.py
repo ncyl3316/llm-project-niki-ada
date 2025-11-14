4594eabd13dd951037d469fd174330f9ae899a21
ncyl3316 <niki2004.chan@gmail.com>
2025-11-15 00:25:15 +0800

    Delete src/evaluation directory

--- src/evaluation/evaluate.py
+++
@@ -1,4 +0,0 @@
-# Module: evaluate.py 
-# Author: Your Name 
-# Date: 29-12-2024 
-# Description: Scripts to validate and assess model performance on various datasets. 
--- src/evaluation/metrics.py
+++
@@ -1,4 +0,0 @@
-# Module: metrics.py 
-# Author: Your Name 
-# Date: 29-12-2024 
-# Description: Defines evaluation metrics for measuring model performance and accuracy. 
